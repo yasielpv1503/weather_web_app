5afd4537b843f46e8646552ba1fa0640154785b3
daenerys targaryen <daenerystargaryen@gmail.com>
2020-09-26 15:10:00 -0400

    add build to deploy

---
+++ build/precache-manifest.0a2046f72449007bd8cc149edacf5a8d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe9a54490fa27f4470acff7ef739bd5",
+    "url": "http://weather.sacavix.com/index.html"
+  },
+  {
+    "revision": "e2da758ad8c38823c17c",
+    "url": "http://weather.sacavix.com/static/css/4.355be238.chunk.css"
+  },
+  {
+    "revision": "4511f15b2e2b4334b9b7",
+    "url": "http://weather.sacavix.com/static/css/main.a6dcec3e.chunk.css"
+  },
+  {
+    "revision": "a454808834dd7c3bd37c",
+    "url": "http://weather.sacavix.com/static/js/0.f72f604a.chunk.js"
+  },
+  {
+    "revision": "9d15881e00082a29e4c8",
+    "url": "http://weather.sacavix.com/static/js/1.a88bf392.chunk.js"
+  },
+  {
+    "revision": "e2da758ad8c38823c17c",
+    "url": "http://weather.sacavix.com/static/js/4.f401ea49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "http://weather.sacavix.com/static/js/4.f401ea49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fef0eaa3cae5d50da4",
+    "url": "http://weather.sacavix.com/static/js/5.a3c310dc.chunk.js"
+  },
+  {
+    "revision": "d677910a89826e72dc74",
+    "url": "http://weather.sacavix.com/static/js/6.b2f1f439.chunk.js"
+  },
+  {
+    "revision": "31b121cc55e0c84b4b16",
+    "url": "http://weather.sacavix.com/static/js/7.4de30157.chunk.js"
+  },
+  {
+    "revision": "4511f15b2e2b4334b9b7",
+    "url": "http://weather.sacavix.com/static/js/main.17dcc51a.chunk.js"
+  },
+  {
+    "revision": "8905321eed2c564e032b",
+    "url": "http://weather.sacavix.com/static/js/runtime-main.3f2525f6.js"
+  }
+]);
